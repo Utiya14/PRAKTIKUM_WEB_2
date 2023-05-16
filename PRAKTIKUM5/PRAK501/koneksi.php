7dcd77671c8e9d24d95962921c1d3610e718caaf
Utiya14 <115009138+Utiya14@users.noreply.github.com>
2023-05-16 11:07:51 +0800

    koneksi.php

--- PRAKTIKUM5/PRAK501/koneksi.php
+++ PRAKTIKUM5/PRAK501/koneksi.php
@@ -1,1 +1,11 @@
-.
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "web2";
+
+$connect = mysqli_connect($servername, $username, $password, $database);
+
+if (!$connect) {
+  die("<script>alert('Gagal tersambung dengan database.')</script>");
+}
